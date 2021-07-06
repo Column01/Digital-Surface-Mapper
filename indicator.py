e1cc41d31f278f0771ead8cd6e8cbaf310f6637a
Column01 <colinandress999@gmail.com>
2021-07-05 20:46:27 -0400

    Add get multiple readings as a list

--- indicator.py
+++ indicator.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 import serial
 import serial.tools.list_ports
 
@@ -7,10 +9,16 @@ class IndicatorReader:
         self.com_port = com_port
         self.baud = baud
         self.ser = None
+        self.RETURN_SEQ = b"\r\n"
     
     def connect(self):
         """ Opens the connection to the indicator """
         self.ser = serial.Serial(port=self.com_port, baudrate=self.baud)
+    
+    def disconnect(self):
+        if self.is_ready():
+            self.ser.close()
+        self.ser = None
 
     def is_ready(self):
         """ Checks if the serial connection is open """
@@ -27,16 +35,24 @@ class IndicatorReader:
         """
         if self.is_ready():
             self.ser.reset_input_buffer()
-            line = self.ser.readline().strip(b"\r\n").decode().split(",")
+            line = self.ser.readline().strip(self.RETURN_SEQ).decode().split(",")
             unit = "in" if line[1] == "1" else "mm"
             value = int(line[0]) / 2000 if unit == "in" else int(line[0]) / 100
             return value, unit
         else:
             return None
+        
+    def get_readings(self, num_readings, time_between):
+        readings = []
+        for _ in range(num_readings):
+            readings.append(self.get_reading())
+            sleep(time_between)
+        
+        return readings
 
 
 if __name__ == "__main__":
-    com_ports = ["- " + comport.device for comport in serial.tools.list_ports.comports()]
+    com_ports = [f"- {comport.device}" for comport in serial.tools.list_ports.comports()]
     print("Choose your com port:")
     print("\n".join(com_ports))
     com_port = input("Type full comport name: ")
